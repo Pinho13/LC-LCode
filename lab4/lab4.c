75c8736febb7dfa6b48bd1d92b17690938cec2f6
Pinho <rafaelpinhotsilva@gmail.com>
2026-04-18 21:09:12 +0100

    Mouse async test working

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -78,7 +78,70 @@ int (mouse_test_packet)(uint32_t cnt) {
 }
 
 int (mouse_test_async)(uint8_t idle_time) {
-    /* To be completed */
-    printf("%s(%u): under construction\n", __func__, idle_time);
-    return 1;
+  uint32_t timer_counter = 0;
+
+  uint8_t mouse_bit_no;
+  uint8_t timer_bit_no;
+  int ipc_status;
+  message msg;
+  int r;
+
+  uint32_t packets_read = 0;
+
+  if (mouse_subscribe_int(&mouse_bit_no) != OK) return 1;
+  if (my_mouse_enable_data_reporting() != OK) return 1;
+
+  if (timer_subscribe_int(&timer_bit_no) != OK) return 1;
+
+  uint8_t mouse_irq_set = BIT(mouse_bit_no);
+  uint8_t timer_irq_set = BIT(timer_bit_no);
+
+
+  while (1) {
+    if ((r = driver_receive(ANY, &msg, &ipc_status)) != OK)
+      continue;
+
+    if (is_ipc_notify(ipc_status)) {
+      switch (_ENDPOINT_P(msg.m_source)) {
+        case HARDWARE:
+          if (msg.m_notify.interrupts & mouse_irq_set) {
+            mouse_ih();
+
+
+            if (is_packet_ready()) {
+              struct packet pp;
+              mouse_parse_packet(&pp);
+              mouse_print_packet(&pp);
+
+              packets_read++;
+              
+              timer_counter = 0;
+            }
+          }
+
+          if (msg.m_notify.interrupts & timer_irq_set) {
+            timer_int_handler();
+
+            timer_counter++;
+
+            if (timer_counter >= idle_time * 60) {
+              if (timer_unsubscribe_int() != OK) return 1;
+              if (my_mouse_disable_data_reporting() != OK) return 1;
+              if (mouse_unsubscribe_int() != OK) return 1;
+              return 0;
+            }
+
+          }
+          break;
+        default:
+          break;
+      }
+    }
+  }
+
+  if (timer_unsubscribe_int() != OK) return 1;
+  if (my_mouse_disable_data_reporting() != OK) return 1;
+  if (mouse_unsubscribe_int() != OK) return 1;
+
+  return 0;
 }
