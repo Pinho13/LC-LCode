a1d21ced6d7f263404946f7247dd2c5572b068d8
Pinho <rafaelpinhotsilva@gmail.com>
2026-05-10 18:19:15 +0100

    Made main use proj service

--- framework/main.c
+++ framework/main.c
@@ -1,5 +1,19 @@
+#include <lcom/lcf.h>
 
+int(proj_main_loop)(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
+  return 0;
+}
 
-int main() {
+int(main)(int argc, char *argv[]) {
+  lcf_set_language("EN-US");
+  lcf_trace_calls("/home/lcom/labs/framework/trace.txt");
+  lcf_log_output("/home/lcom/labs/framework/output.txt");
+
+  if (lcf_start(argc, argv))
+    return 1;
+
+  lcf_cleanup();
   return 0;
 }
