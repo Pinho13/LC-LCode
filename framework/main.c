2e4dcd51918dc87ed0768b17b5b1d86d301c5bc8
Pinho <rafaelpinhotsilva@gmail.com>
2026-05-12 16:34:23 +0100

    Added i8042.h and changed main

--- framework/main.c
+++ framework/main.c
@@ -1,4 +1,45 @@
 #include <lcom/lcf.h>
+#include <minix/syslib.h>
+#include <minix/sysutil.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+#include "fw/drivers/rtc.h"
+
+#define ASSERT(x, msg)                                                         \
+  if (!(x)) {                                                                  \
+    printf("%s FAILED!\n", msg);                                               \
+    exit(1);                                                                   \
+  } else {                                                                     \
+    printf("%s passed.\n", msg);                                               \
+  }
+
+
+int rtc_example() {
+  rtc_date date = {0, 0, 0};
+  if (rtc_read_date(&date) != OK) return 1;
+
+  printf("\nday: %u \n", date.day);
+  printf("month: %u \n", date.month);
+  printf("year: %u \n", date.year);
+
+  return 0;
+}
+
+void test_rtc_date() {
+  rtc_date date = {0, 0, 0};
+  rtc_read_date(&date);
+
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
+  struct tm *tm_info = localtime(&ts.tv_sec);
+
+  ASSERT(abs((int)date.day - (int)tm_info->tm_mday) < 2, "rtc read day");
+  ASSERT(abs((int)date.month - (int)tm_info->tm_mon) < 2, "rtc read month");
+  ASSERT(date.year == tm_info->tm_year % 100, "rtc read year");
+}
 
 int(proj_main_loop)(int argc, char *argv[]) {
   (void)argc;
@@ -13,6 +54,9 @@ int(main)(int argc, char *argv[]) {
 
   if (lcf_start(argc, argv))
     return 1;
+  
+  rtc_example();
+  test_rtc_date();
 
   lcf_cleanup();
   return 0;
