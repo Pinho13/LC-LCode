9e13dc910cf87e5501a6c9baa8b7631626e5068f
Pinho <rafaelpinhotsilva@gmail.com>
2026-05-17 15:56:29 +0100

    Added error messages to rtc

--- framework/src/drivers/rtc.c
+++ framework/src/drivers/rtc.c
@@ -1,4 +1,5 @@
 #include "fw/drivers/rtc.h"
+#include "fw/common/utils.h"
 
 static int bcd_to_bin(uint8_t bcd) {
   uint8_t ones = bcd & 0x0F;
@@ -11,35 +12,38 @@ static int rtc_read_reg(uint8_t reg, uint8_t *val) {
   uint32_t tmp;
 
   if (sys_outb(RTC_ADDR_REG, reg) != OK)
-    return 1;
+    return fail(ERR_RTC, "rtc_read_reg: failed to write RTC address register");
 
   if (sys_inb(RTC_DATA_REG, &tmp) != OK)
-    return 1;
+    return fail(ERR_RTC, "rtc_read_reg: failed to read RTC data register");
 
   *val = (uint8_t) tmp;
   return 0;
 }
 
 int rtc_read_date(rtc_date *date) {
-  if (!date) return 1;
+  if (!date) return fail(ERR_RTC, "rtc_read_date: NULL date pointer");
 
   uint8_t regA, regB, day, month, year;
 
   do {
     tickdelay(micros_to_ticks(50));
     if (rtc_read_reg(RTC_REG_A, &regA) != OK)
-      return 1;
+      return fail(ERR_RTC, "rtc_read_date: failed to read register A");
   } while (regA & RTC_UIP_MSK);
 
 
   if (rtc_read_reg(RTC_REG_B, &regB) != OK)
-    return 1;
+    return fail(ERR_RTC, "rtc_read_date: failed to read register B");
 
   bool isBCD = !(regB & RTC_DM_MSK);
 
-  if (rtc_read_reg(RTC_REG_DAY, &day) != OK) return 1;
-  if (rtc_read_reg(RTC_REG_MONTH, &month) != OK) return 1;
-  if (rtc_read_reg(RTC_REG_YEAR, &year) != OK) return 1;
+  if (rtc_read_reg(RTC_REG_DAY, &day) != OK)
+    return fail(ERR_RTC, "rtc_read_date: failed to read day");
+  if (rtc_read_reg(RTC_REG_MONTH, &month) != OK)
+    return fail(ERR_RTC, "rtc_read_date: failed to read month");
+  if (rtc_read_reg(RTC_REG_YEAR, &year) != OK)
+    return fail(ERR_RTC, "rtc_read_date: failed to read year");
 
   if (isBCD) {
     day = bcd_to_bin(day);
