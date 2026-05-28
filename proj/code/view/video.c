7603005cb1831ab2341e67b44a6180fce6807cdf
Miguel Rocha <miguelmirandarocha@gmail.com>
2026-05-28 22:09:02 +0100

    Efficiency improvement on drawing

--- proj/code/view/video.c
+++ proj/code/view/video.c
@@ -41,16 +41,14 @@ void video_cleanup() {
 
 void bb_draw_pixel(uint16_t x, uint16_t y, uint32_t color) {
   if (x >= vg_get_h_res() || y >= vg_get_v_res()) return;
-
-  unsigned bpp = vg_get_bytes_per_pixel();
-  unsigned index = (vg_get_h_res() * y + x) * bpp;
-  memcpy(back_buffer + index, &color, bpp);
+  ((uint32_t *)back_buffer)[y * vg_get_h_res() + x] = color;
 }
 
 void bb_draw_hline(uint16_t x, uint16_t y, uint16_t len, uint32_t color) {
-  for (uint16_t i = 0; i < len; i++) {
-    bb_draw_pixel(x + i, y, color);
-  }
+  if (y >= vg_get_v_res() || x >= vg_get_h_res()) return;
+  if (x + len > vg_get_h_res()) len = vg_get_h_res() - x;
+  uint32_t *p = (uint32_t *)back_buffer + y * vg_get_h_res() + x;
+  for (uint16_t i = 0; i < len; i++) p[i] = color;
 }
 
 void bb_draw_rect(uint16_t x, uint16_t y, uint16_t w, uint16_t h, uint32_t color) {
@@ -74,10 +72,7 @@ void bb_clear(uint32_t color) {
     memset(back_buffer, 0, vg_get_frame_size());
     return;
   }
-
-  unsigned bpp = vg_get_bytes_per_pixel();
-  unsigned pixels = vg_get_h_res() * vg_get_v_res();
-  for (unsigned i = 0; i < pixels; i++) {
-    memcpy(back_buffer + i * bpp, &color, bpp);
-  }
+  uint32_t *p = (uint32_t *)back_buffer;
+  unsigned total = vg_get_h_res() * vg_get_v_res();
+  for (unsigned i = 0; i < total; i++) p[i] = color;
 }
