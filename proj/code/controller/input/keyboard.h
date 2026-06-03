6af3704ec92142f35314e73f1788ed98869ab544
Pinho <rafaelpinhotsilva@gmail.com>
2026-06-03 17:39:32 +0100

    Refactor keyboard_process

--- proj/code/controller/input/keyboard.h
+++ proj/code/controller/input/keyboard.h
@@ -1,20 +1,29 @@
 #pragma once
 
 #include <lcom/lcf.h>
+#include <stdbool.h>
 
 #include "fw/drivers/keyboard.h"
 
-typedef enum { DIR_NONE, DIR_LEFT, DIR_RIGHT, DIR_UP, DIR_DOWN, DIR_HOME, DIR_END } Direction;
+typedef enum {
+    DIR_NONE,
+    DIR_LEFT,
+    DIR_RIGHT,
+    DIR_UP,
+    DIR_DOWN,
+    DIR_HOME,
+    DIR_END
+} Direction;
 
 typedef struct {
-  char c;
-  bool ctrl;
-  bool shift;
-  bool altgr;
-  bool backspace;
-  bool enter;
-  bool escape;
-  Direction dir;
+    char c;
+    bool ctrl;
+    bool shift;
+    bool altgr;
+    bool backspace;
+    bool enter;
+    bool escape;
+    Direction dir;
 } KeyEvent;
 
 void keyboard_process(packet_scancode ps);
